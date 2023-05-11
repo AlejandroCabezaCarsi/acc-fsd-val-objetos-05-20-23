ca9c76d686e128fb026f690317b31e406e796275
Alex <cabezacarsialejandro@gmail.com>
2023-05-11 12:57:18 +0200

    Class

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// let dias = [1,88,'jose',true,7.89,[5,9,6]]; 
+
+// console.log(dias[0]); 
+// console.log(dias[5][1]);
+
+//Informacion alumnos 
+
+// let alumnos = [['nombre', 'apellido','dni'],
+//     ['nombre', 'apellido','dni'],
+//     ['nombre', 'apellido','dni'],
+//     ['nombre', 'apellido','dni'],
+//     ['nombre', 'apellido','dni'],
+// ];
+
+// alumnos.push(['jose', 'marin', '2143923a']);
+
+// console.log(alumnos);
+
+
+//objetos literales JS
+
+// let alumnos = { 
+//     'nombre': 'jose',
+//     'apellido': 'marin'
+
+// };
+
+// console.log(alumnos.nombre);
+
+
+//Array de objetos
+
+let alumnos = [
+    {
+        'nombre':'jose',
+        'apellido':'marin'
+    },
+    {
+        'nombre':'Mara',
+        'apellido':'marin'
+    },
+    {
+        'nombre':'Marin',
+        'apellido':'cabeza'
+    },
+];
+
+console.log(alumnos[2].apellido);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
